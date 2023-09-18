48c4af6d4d2db4c543aa7ac8e4c05d27a614c127
DiegoPSP <diego.paulo2022@gmail.com>
2023-09-18 14:11:14 -0300

    Operadores de atibuição e comparação com exemplos.

---
+++ operadores_atribuicao_comparacao.py
@@ -0,0 +1,32 @@
+
+#Operadores de comparação
+saldo = 100
+saque = 200
+
+#Compara e retorna "True" ou "False" a depender da comparação;
+print (saque < saldo)
+
+print(saldo == saque)
+
+
+#Operadores de atribuição simples
+saldo = 100
+print(saldo)
+
+#Operadores de atribuição composta. Faça a operação e coloque o sinal de "=" antes do número ou condição;x'
+#A atribuição composta ela resgata o valor da variável e soma, subtrai, divide, multiplica e etc... 
+# Qualque que seja a expressão, você realiza operações com o valor inicial da variável;
+saldo += 100
+print(saldo)
+saldo -= 50
+print(saldo)
+saldo /= 2
+print(saldo)
+saldo *= 4
+print(saldo)
+saldo **= 1
+print(saldo)
+saldo //= 2
+print(saldo)
+saldo %= 7
+print(saldo)
