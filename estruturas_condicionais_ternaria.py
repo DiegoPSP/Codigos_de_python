9978e870b0099fa7f8684c1fe849ea8856554254
DiegoPSP <diego.paulo2022@gmail.com>
2023-09-19 11:54:25 -0300

    Tipo de estrutura de condicional que se resolve em apenas 1 linha

--- estruturas_condicionais_ternaria.py
+++ estruturas_condicionais_ternaria.py
@@ -1,9 +1,10 @@
 saldo = 2000
 saque = 5000
-#Essa condicional é composta por 3 partes;
+#Essa condicional é composta por 3 partes:
 # 1- a variável e a mensagem a ser exibida caso for verdadeiro;
 # 2- a condição lógica, nesse caso saldo ser maior ou igual a saque;
 # 3- a segunda mensagem caso retorne falso;
+#O diferencial dela é poder fazer em apenas uma linha quando precisa de respostas simples e funcionais;
 status = "Sucesso" if saldo >= saque else "Falha"
 
 print(f"{status} ao realizar o saque!")
