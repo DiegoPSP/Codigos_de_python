f2e8e1c4aa04c298734e3a971dd9655500da7589
DiegoPSP <diego.paulo2022@gmail.com>
2023-09-25 10:45:46 -0300

    Adicionei comentários para ficar mais fácil de entender.

--- 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
+++ 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
@@ -1,14 +1,14 @@
-frutas = ["laranja", "maca", "uva"]
+frutas = ["laranja", "maca", "uva"]#Posso declarar com objetos já pré-definidos;
 print(frutas)
 
-frutas = []
+frutas = []#Posso declarar uma lista vazia;
 print(frutas)
 
-letras = list("python")
-print(letras)
+letras = list("python")#Posso declarar utilizando a função list, mas se fizer dessa maneira aí cada letra será um objeto diferente;
+print(letras)#O programa entende que "p" é um objeto, "y" é outro objeto e assim por diante;
 
-numeros = list(range(10))
-print(numeros)
+numeros = list(range(10))#Posso declarar com o range que é um "contador", ele cria elementos de acordo com o número que você declarou;
+print(numeros)#Essa é a função build-in, vai criar um elemento para cada elemento retornado pela função range;
 
-carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]#Posso declarar com vários objetos, idependente do seu tipo;
 print(carro)
