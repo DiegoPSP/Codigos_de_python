8a2842faaef3013aa982af91164827be17635073
DiegoPSP <diego.paulo2022@gmail.com>
2023-09-25 16:21:34 -0300

    c:; cd 'c:\Users\Dell Inspiron\OneDrive\Documentos\Codigos_de_python'; & 'C:\Users\Dell Inspiron\AppData\Local\Programs\Python\Python311\python.exe' 'c:\Users\Dell Inspiron\.vscode\extensions\ms-python.python-2023.16.0\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '50839' '--' 'c:\Users\Dell Inspiron\OneDrive\Documentos\Codigos_de_python\01 - Estrutura de dados\01 - Listas\04_fatiamento.py'

--- 01 - Estrutura de dados/01 - Listas/01_acesso_direto.py
+++ 01 - Estrutura de dados/01 - Listas/01_acesso_direto.py
@@ -2,3 +2,5 @@ frutas = ["maçã", "laranja", "uva", "pera"]
 
 print(frutas[0])  # maçã
 print(frutas[2])  # uva
+#Para pegar um elemento da lista de forma direta, basta fazer a chamada da variável e entre colchetes colocar;
+# a posição que ele está;
