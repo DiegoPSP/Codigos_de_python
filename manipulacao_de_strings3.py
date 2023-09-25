4da384f268e07c9542e603d0254b74d7ff87218d
DiegoPSP <diego.paulo2022@gmail.com>
2023-09-24 22:45:04 -0300

    Fatiamento de string e como espelhar a string

---
+++ manipulacao_de_strings3.py
@@ -0,0 +1,10 @@
+nome = "Diego Pereira Soares de Paulo"
+
+print(nome[0])#Pega o primeiro caracter;
+print(nome[-1])#Pega o último caracter;
+print(nome[:13])#Vai do início até o número de caracter que foi passado como parêmetro;
+print(nome[6:])#Mostra a partir do número de caracter que foi passado como parâmetro;
+print(nome[6:13])#Junção dos dois que foram explicados acima;
+print(nome[6:13:3])#Pega os caracteres e divide pelo parâmetro que foi passado;
+print(nome[:])#Replica a string;
+print(nome[:: -1])#Espelha a string;
